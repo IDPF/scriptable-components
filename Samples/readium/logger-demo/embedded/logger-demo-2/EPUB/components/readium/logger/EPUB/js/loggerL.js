c4fcb1ad1dd0c0b468cbb6164c7a9fe6129e4470
Ric Wright <rkwright@geofx.com>
2015-07-13 12:02:02 -0700

    Miscellaneous cleanup and formatting.

--- Samples/readium/logger-demo/embedded/logger-demo-2/EPUB/components/readium/logger/EPUB/js/loggerL.js
+++ Samples/readium/logger-demo/embedded/logger-demo-2/EPUB/components/readium/logger/EPUB/js/loggerL.js
@@ -1,7 +1,7 @@
 
 
 // Get a reference to the <div> on the page that will display the message text.
-var loggerDiv = document.getElementById('loggerDiv');
+var loggerDiv = document.getElementById('loggerLDiv');
 
 window.onload = function() {
 	console.log('The child-window logger JS got loaded!');
@@ -41,6 +41,36 @@ window.onload = function() {
     	elm.scrollTop = elm.scrollHeight;
 	}
 
+    /**
+     * Pad an integer with the specified number of leading zeroes.
+     *
+     * @param num
+     * @param places
+     * @returns {string}
+     */
+    function zeroPad(num, places) {
+        var zero = places - num.toString().length + 1;
+        return Array(+(zero > 0 && zero)).join("0") + num;
+    }
+
+    /**
+     *  Return a nicely formatted, UNIX-style string
+     *
+     * @returns {string}
+     */
+    function formatTimeString( date ) {
+        var hours = zeroPad(date.getHours(),2);
+        var minutes = zeroPad(date.getMinutes(),2);
+        var seconds = zeroPad(date.getSeconds(),2);
+        var ms = zeroPad(date.getMilliseconds(),3);
+        var day = zeroPad(date.getDate(), 2);
+        var month = zeroPad((date.getMonth() + 1),2);
+        var year = date.getFullYear().toString();
+        var timeDate = year + month + day + "-" + hours + ":" + minutes + ":" + seconds + "." + ms;
+
+        return timeDate;
+    }
+
     function getCurTempData () {
 
         var data = {
@@ -54,7 +84,8 @@ window.onload = function() {
 
 	setInterval(function() {
         var data = getCurTempData();
-        logMsg( loggerDiv, "Widget LoggerL publishing [tempwave]: " + "temperature: " + data.currentTemp + " at " +  data.time);
+        var time = formatTimeString( new Date());
+        logMsg( loggerDiv, "Widget LoggerL publishing [tempwave]: " + "temp: " + data.currentTemp + " at " +  time);
         epubsc.publish("tempwave", data);
 	},2000);
 
--- Samples/readium/logger-demo/embedded/logger-demo-2/EPUB/components/readium/logger/EPUB/js/loggerR.js
+++ Samples/readium/logger-demo/embedded/logger-demo-2/EPUB/components/readium/logger/EPUB/js/loggerR.js
@@ -1,9 +1,39 @@
 
 // Get a reference to the <div> on the page that will display the message text.
-var loggerDiv = document.getElementById('loggerDiv');
+var loggerDiv = document.getElementById('loggerRDiv');
 
 window.onload = function() {
 	console.log('The child-window logger JS got loaded!');
+
+    /**
+     * Pad an integer with the specified number of leading zeroes.
+     *
+     * @param num
+     * @param places
+     * @returns {string}
+     */
+    function zeroPad(num, places) {
+        var zero = places - num.toString().length + 1;
+        return Array(+(zero > 0 && zero)).join("0") + num;
+    }
+
+    /**
+     *  Return a nicely formatted, UNIX-style string
+     *
+     * @returns {string}
+     */
+    function formatTimeString( date ) {
+        var hours = zeroPad(date.getHours(),2);
+        var minutes = zeroPad(date.getMinutes(),2);
+        var seconds = zeroPad(date.getSeconds(),2);
+        var ms = zeroPad(date.getMilliseconds(),3);
+        var day = zeroPad(date.getDate(), 2);
+        var month = zeroPad((date.getMonth() + 1),2);
+        var year = date.getFullYear().toString();
+        var timeDate = year + month + day + " " + hours + ":" + minutes + ":" + seconds + "." + ms;
+
+        return timeDate;
+    }
 
 	// subscribe to the epubsc event call
 	epubsc.subscribe("epubsc_event", function(msg){
@@ -31,8 +61,10 @@ window.onload = function() {
 		logMsg( loggerDiv, "Topic: [unload] " + " ESC: "+ msg.data.widgetId);
 	});
 
-	epubsc.subscribe("tempwave", function(msg){
-		logMsg( loggerDiv, "Topic: [tempwave] " + " ESC: "+ msg.data.widgetId + " temperature: " + msg.data.topicData.currentTemp + " at: " + msg.data.topicData.time);
+	epubsc.subscribe("tempwave", function(msg) {
+        var ID = msg.data.widgetId;
+        var time = formatTimeString( new Date(msg.data.topicData.time));
+		logMsg( loggerDiv, "Topic: [tempwave] " + " ESC: "+ ID.substr(0,9) + ",  temp: " + msg.data.topicData.currentTemp + " at: " + time);
 	});
 
 	function logMsg( elm, msg ) {
